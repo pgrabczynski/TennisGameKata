dcce755d61bfd749ef82c01f80f4842bf44d64b5
Patryk <grabczynski.patryk@gmail.com>
2020-05-23 14:33:28 +0200

    test 0

--- src/test/java/TennisGameKataTest.java
+++ src/test/java/TennisGameKataTest.java
@@ -1,5 +1,20 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pl.patryk.TennisGame;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TennisGameKataTest {
+
+
+    @Test
+    public void shouldScoreBeZero(){
+        TennisGame tennisGame = new TennisGame();
+
+        assertEquals(0, tennisGame.getScore());
+    }
+
+
+
 
 }
---
+++ src/test/java/pl/patryk/TennisGame.java
@@ -0,0 +1,16 @@
+package pl.patryk;
+
+public class TennisGame {
+
+    public TennisGame() {
+
+    }
+
+    public int getScore(){
+        return 0;
+
+    }
+
+
+
+}
